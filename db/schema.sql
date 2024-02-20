480cd2b43c17218df15a5b3b7a38291ff33082c3
hieuung <ungtrunghieu99@gmail.com>
2024-02-20 18:04:10 +0700

    edit

--- db/schema.sql
+++ db/schema.sql
@@ -12,22 +12,4 @@ CREATE TABLE webapp.app_users (
   updated_time BIGINT NOT NULL 
 );
 
--- CREATE OR REPLACE PROCEDURE sp_createUser(
---     p_name VARCHAR(20),
---     p_email VARCHAR(20),
---     p_password VARCHAR(255),
--- 	p_created_time BIGINT,
--- 	p_updated_time BIGINT
--- )
--- LANGUAGE plpgsql
--- AS
--- $$
--- BEGIN
---     IF EXISTS (SELECT 1 FROM webapp.app_users WHERE user_name = p_name AND user_email = p_email) THEN
---         RAISE NOTICE 'User Exists !!';
---     ELSE
---         INSERT INTO webapp.app_users (user_name, user_email, user_password, created_time, updated_time)
---         VALUES (p_name, p_email, p_password, p_created_time, p_updated_time);
---     END IF;
--- END;
--- $$
+CREATE DATABASE datalake;
